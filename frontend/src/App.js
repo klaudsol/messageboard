2b3f14b946c88ecf890efb4129d99f0823642ed5
Ardee Aram <ardee.aram@gmail.com>
2021-05-07 22:50:21 +0000

    React form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Container>
+          <Form>
+    <Form.Group controlId="formBasicEmail">
+      <Form.Label>Name</Form.Label>
+      <Form.Control placeholder="Enter your name" />
+    </Form.Group>
+  
+    <Form.Group controlId="formBasicPassword">
+      <Form.Label>Message</Form.Label>
+      <Form.Control as="textarea" className="text-muted" rows={10} placeholder="Enter your message here." />
+    </Form.Group>
+    <Form.Group>
+      <Button className="mt-3 text-right" variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form.Group>
+  </Form>
+          </Container>
+          
+        <Container className='mt-5'>
+        {[...Array(10)].map((x) => (          
+          <Container className='p-4 mb-1 border border-muted rounded'>
+            <Row>
+              Name
+            </Row>
+            <Row>
+              Message 
+            </Row>
+          </Container>
+         ))}
+        </Container>          
       </header>
     </div>
   );
